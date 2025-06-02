ae6212749c83031dd93c24df89d5c7225fec4b10
guustavomc <gustavomccb@gmail.com>
2025-06-02 08:12:01 -0300

    Files added

--- src/main/java/com/example/currencytracker/controller/ExchangeRateController.java
+++ src/main/java/com/example/currencytracker/controller/ExchangeRateController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/exchange")
-@Validated
 public class ExchangeRateController {
 
     private ExchangeRateService exchangeRateService;
--- src/main/java/com/example/currencytracker/repository/ExchangeRateRepository.java
+++ src/main/java/com/example/currencytracker/repository/ExchangeRateRepository.java
@@ -1,9 +1,11 @@
 package com.example.currencytracker.repository;
 
+import com.example.currencytracker.model.ExchangeRate;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class ExchangeRateRepository {
+public interface ExchangeRateRepository extends JpaRepository<ExchangeRate, Long> {
 
 
 }
